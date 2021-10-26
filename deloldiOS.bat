be6983f35cea3ca35b988cb1d74de033c8441787
theatomus <eranarticus@gmail.com>
2021-10-26 13:08:02 -0400

    Create deloldiOS.bat
    Main deletion file, deletes the old versions

---
+++ deloldiOS.bat
@@ -0,0 +1,29 @@
+@echo off
+pushd "%~dp0"
+start "" /b /wait "files_iPhone Software Updates\ip12.bat"
+start "" /b /wait "files_iPhone Software Updates\ip12m.bat"
+start "" /b /wait "files_iPhone Software Updates\ip12p.bat"
+start "" /b /wait "files_iPhone Software Updates\ip12pm.bat"
+start "" /b /wait "files_iPhone Software Updates\iPhone_78.bat"
+start "" /b /wait "files_iPhone Software Updates\iPhone_78p.bat"
+start "" /b /wait "files_iPhone Software Updates\iPhone_78p.bat"
+start "" /b /wait "files_iPhone Software Updates\iphonese.bat"
+start "" /b /wait "files_iPhone Software Updates\iPhonex.bat"
+start "" /b /wait "files_iPhone Software Updates\iphonexs.bat"
+start "" /b /wait "files_iPhone Software Updates\ipxr.bat"
+
+start "" /b /wait "files_iPad Software Updates\iPad_64bit_TouchID_.bat"
+start "" /b /wait "files_iPad Software Updates\iPad_64bit_TouchID_ASTC_.bat"
+start "" /b /wait "files_iPad Software Updates\iPad_Educational_.bat"
+start "" /b /wait "files_iPad Software Updates\iPad_Educational_2020_.bat"
+start "" /b /wait "files_iPad Software Updates\iPad_Fall_2020_.bat"
+start "" /b /wait "files_iPad Software Updates\iPad_Pro_A12X_A12Z.bat"
+start "" /b /wait "files_iPad Software Updates\iPad_Pro_HFR_.bat"
+start "" /b /wait "files_iPad Software Updates\iPad_Spring_2019_.bat"
+start "" /b /wait "files_iPad Software Updates\iPad8_.bat"
+start "" /b /wait "files_iPad Software Updates\ipadpro_9.bat"
+start "" /b /wait "files_iPad Software Updates\ipadpro_12.bat"
+
+start "" /b /wait "files_iPod Software Updates\iPodtouch_7.bat"
+popd
+exit
